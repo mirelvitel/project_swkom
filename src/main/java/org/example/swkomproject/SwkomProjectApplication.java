b9a68b4e6b3b8295c7f8e194cf5cb01f602ba17b
Ivan Merdzhanov <merdzhanovivan.2302@gmail.com>
2024-09-23 18:39:46 +0200

    dummy response

--- src/main/java/org/example/swkomproject/SwkomProjectApplication.java
+++ src/main/java/org/example/swkomproject/SwkomProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = "org.example.swkomproject.cors")
+@SpringBootApplication(scanBasePackages = {"org.example.swkomproject.cors", "org.example.swkomproject.components"})
 public class SwkomProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(SwkomProjectApplication.class, args);
